ba878340526f944560ba6f1d65efe4e3029512af
Shobhun Shah <shobhunshah321@gmail.com>
2026-06-01 21:17:09 +0530

    Integrate react-native-config and migrated Supabase credentials to environment variables

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import Config from 'react-native-config';
 
-const supabase = createClient(
-  'https://kmisizbukwktqgronokf.supabase.co',
-  'sb_publishable_hT0XwKQxR91GZfikpSolFQ_7HlQ7TTq',
-);
+const supabase = createClient(Config.SUPABASE_URL, Config.SUPABASE_ANON_KEY);
 
 export default supabase;
